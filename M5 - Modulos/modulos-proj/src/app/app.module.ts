7f2bdbed5ba0183c323090b214c896efc8adeb87
Arthur Ramos <thur.trois@gmail.com>
2023-04-01 16:38:56 -0300

    [FEAT] Exemplo M5 - modulos configurado

--- M5 - Modulos/modulos-proj/src/app/app.module.ts
+++ M5 - Modulos/modulos-proj/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ListaModule} from './modules/lista/lista.module'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
